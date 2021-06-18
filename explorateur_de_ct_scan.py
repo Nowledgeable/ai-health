860da3fcb9639030311e35d82b90b067061ab99f
tbnmac <nmac@nowledgeable.com>
2021-06-18 12:42:20 +0200

    fill the first code

--- explorateur_de_ct_scan.py
+++ explorateur_de_ct_scan.py
@@ -1,37 +1,61 @@
 
-import streamlit as stp
+import streamlit as st
 import imageio
+import numpy as np
+
+
+
+def normalise_data(volume, min_intensity = -1000, max_intensity = 200):
+    volume = np.clip(volume, min_intensity, max_intensity)
+    volume = (255*((volume - min_intensity)/(max_intensity - min_intensity))).astype(np.uint8)
+    return volume
+  
 
 
 def display_ct_scan_interface(ct_scan):
     """
         Show a CT scan section dependending on user options
     """
-    tranche = st.radio('tranche', ['axial', 'coaxial', 'etc'] )
+    options = ['axial', 'coronal', 'sagittal'] 
+    tranche = st.radio('tranche', options)
+
+    #print(options.index[tranche])
 
     slider_min = 0
-    slider_max = ct_scan.shape[tranche]
+    slider_max = ct_scan.shape[options.index(tranche)]
 
     section = st.slider("section of ct_scan", slider_min, slider_max-1)
     
-    if tranche == 0:
+    if tranche == 'axial':
         image = ct_scan[section]
-    elif tranche == 1:
+    elif tranche == 'coronal':
         image = ct_scan[:, section, :]
     else: 
         image = ct_scan[:, :, section]
 
-    st.image(, channels="BGR")
+    print("type of image", type(image))
+
+    image = np.array(image)
+    image = normalise_data(image)
+
+    st.image(image)
+
+
+
 
 
 st.title('CT scan explorer')
-st.write('Please pick a folder !')
-folder = st.file_uploader()
+#st.write('Please pick a folder !')
+folder = st.file_uploader(label =" Please pick a folder", accept_multiple_files=True)
+folder = "/Users/bnmac/Documents/Nowledgable/MonAi/ct_scan/train/ID00180637202240177410333"
+
+print('folder', folder)
 
 @st.cache
 def load_ct_scan(folder):
-
-    return  imageio.load(folder)
+    image = imageio.volread(folder)
+    print("image shape", image.shape )
+    return image
 
 
 if (folder):
